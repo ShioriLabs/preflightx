5940205d67cff047d9076704c76b9a64d86439ad
Dhika Rizky <deka_r@outlook.com>
2023-08-12 01:04:15 +0700

    1.2.0
    * Updated normalize to modern-normalize
    
    * Updating readme and package version
    
    * Package chores
    
    * Update dependencies
    
    * Changed styling solution to Tailwind

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,17 @@
 import React from 'react'
 import Head from 'next/head'
-import { ThemeProvider } from 'styled-components'
 import { AppProps } from 'next/dist/shared/lib/router/router'
 
-import 'modern-normalize'
+import '../styles/global.css'
 
 export default function App ({ Component, pageProps }: AppProps): React.ReactElement {
   return (
     <>
-      <ThemeProvider theme={{}}>
-        <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-        </Head>
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+      </Head>
+      <Component {...pageProps} />
     </>
   )
 }
